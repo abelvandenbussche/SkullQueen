0ec93aabadafff6663911c4406aadbbcde9f594c
Abel Vandenbussche <abel.vandenbussche@leerling.pti.be>
2026-04-01 10:04:22 +0200

    add onto player + update ui

--- SkullQueen/Player.cs
+++ SkullQueen/Player.cs
@@ -5,5 +5,19 @@
         private Plank plank;
         private List<Card> hand;
         private int score;
+        string name;
+
+        public Player(string name)
+        {
+            this.name = name;
+
+            this.plank = new Plank();
+            this.score = 0;
+            this.hand = new List<Card>();
+        }
+        public void ReceiveCard(Card card)
+        {
+            hand.Add(card);
+        }
     }
 }
