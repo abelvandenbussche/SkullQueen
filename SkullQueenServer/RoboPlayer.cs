c0690e4da2f5b78251074a1db83d4f727966cc57
abelvandenbussche <abel.vandenbussche@gmail.com>
2026-04-13 21:48:10 +0200

    make start of ai player

---
+++ SkullQueenServer/RoboPlayer.cs
@@ -0,0 +1,37 @@
+﻿using Shared;
+
+namespace SkullQueenServer
+{
+    class RoboPlayer : Player
+    {
+        public RoboPlayer(Random rand) : base(RoboPlayer.GenerateName(rand), null) { }
+
+        public static string GenerateName(Random rand)
+        {
+            return "robot_captain_v" + rand.Next(10000);
+        }
+
+        private Plank SetupPlank()
+        {
+            Dictionary<Color, int> OffsetPerColor = new()
+            {
+                {Color.Red, 0},
+                {Color.Green, 0},
+                {Color.Blue, 0},
+                {Color.Yellow, 0},
+            };
+            foreach (Card card in hand)
+            {
+                OffsetPerColor[card.suit] += card.rank > 6.5 ? 1 : -1;
+            }
+            Plank plank = new(
+                Math.Max(Math.Min(OffsetPerColor[Color.Red], 2), -2),
+                Math.Max(Math.Min(OffsetPerColor[Color.Green], 2), -2),
+                Math.Max(Math.Min(OffsetPerColor[Color.Yellow], 2), -2),
+                Math.Max(Math.Min(OffsetPerColor[Color.Blue], 2), -2),
+                OffsetPerColor.Values.Sum() > 0
+            );
+            return plank;
+        }
+    }
+}
