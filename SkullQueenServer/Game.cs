ceff0aa1df5d06ee59eff9fd32507738a92db1a7
abelvandenbussche <abel.vandenbussche@gmail.com>
2026-05-16 13:31:32 +0200

    fix starting player to switch after the round not before

--- SkullQueenServer/Game.cs
+++ SkullQueenServer/Game.cs
@@ -83,13 +83,13 @@ namespace SkullQueenServer
         {
             for (int i = 0; i < players.Count; i++)
             {
+              this.currentRound = new Round(players);
+              await currentRound.StartRound();
+
               //Rotating the players so the start players is different
               Player first = players[0];
               players.RemoveAt(0);
               players.Add(first);
-
-              this.currentRound = new Round(players);
-              await currentRound.StartRound();
             }
             // Sending the scores of the players for the end of the game
             string message = "";
